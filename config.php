2065d96fdaa12498b879e363be3b2d0b57ea6514
Whisperity <whisperity@gmail.com>
2010-05-23 13:40:05 +0000

    Some new config array elements

--- config.php
+++ config.php
@@ -16,6 +16,20 @@
 	'dbpass' => '', // Jelszó
 	'dbname' => '', // Adatbázis neve
 	'tbprf' => '', // Táblanév prefixum
+	
+	/* Levélküldési beállítások */
+	'SMTP' => "", // SMTP szerver neve (e-mailek küldéséhez)
+	'smtp_port' => 25, // SMTP port szám (alapértelmezett: 25)
+	'sendmail_from' => '', // A portál által küldött e-mailek feladó címe
+	'sendmail_html' => 1, // HTML-stílusú levél küldése (1-igen, 0-nem (szöveges levél küldése))
+	
+	/* Weboldal adatai */
+	'phost' => '', // Webcím (domain)
+	'pname' => '', // Weboldalnév
+	
+	/* Webmester */
+	'webmaster' => '', // Webmester neve
+	'webmaster_email' => '', // Webmester e-mail címe
  );
  
  define('THEME_NAME', 'default'); // Téma neve
