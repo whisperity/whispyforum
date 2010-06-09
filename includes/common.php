834ea80dd869926e9cdd0e0105d02c6329e50c9b
Whisperity <whisperity@gmail.com>
2010-06-09 19:44:26 +0000

    Redone footer output Now giving the time in seconds that took to generate the website If the page version != setupped version, the link to update help is being pop-up by JavaScript.

--- includes/common.php
+++ includes/common.php
@@ -19,16 +19,32 @@ function Fejlec()
 function Lablec()
 {
 	// Lábléc létrehozása
-	print("<div class='footerbox'>"); // Blokknyitás
+	print("<div class='menubox'><span class='menutitle'>Információk</span><br><p class='menuItem'>"); // Blokknyitás
+	/* Generálás vége, generálási idő kiszámítása */
+	global $start_time;
 	
-	// KÓD IDE //
-	print("</div>"); // Blokkzárás
+    $mtime = microtime();
+	$mtime = explode(' ',$mtime);
+    $current_time = $mtime[1] + $mtime[0];
+	
+	$genIdo = substr(($current_time - $start_time), 0, 5);
+	
+	print("<a href=''>WhispyFórum " .RELEASE_TYPE. " " .VERSION. " (" .RELEASE_DATE. ")</a>");
+	print("<br>A generálás " .$genIdo. " másodpercig tartott");
+	
+	print("</p></div>"); // Blokkzárás
 }
 
 function Inicialize ( $pagename )
 {
+ /* Generálás kezdete, idő eltárolása */
+ global $start_time;
+ $mtime = microtime();
+ $mtime = explode(' ',$mtime);
+ $start_time = $mtime[1] + $mtime[0];
+ 
  session_start(); // Elindítjuk a munkafolyamatot
-  
+
  /* SZÜKSÉGES FÁJLOK BETÖLTÉSE */
  require('config.php'); // Konfigurációs állomány betöltése
  require('includes/versions.php'); // Verzióinformációk
@@ -41,7 +57,8 @@ function Inicialize ( $pagename )
  require('includes/templates.php'); // Modulkezelő
  
  // Témafájl betöltése
- print("<link rel='stylesheet' type='text/css' href='themes/" .THEME_NAME. "/style.css'>");
+ print("<link rel='stylesheet' type='text/css' href='themes/" .THEME_NAME. "/style.css'>
+");
  /* */
   
  /* INICIALIZÁLÁS */
@@ -55,9 +72,11 @@ function Inicialize ( $pagename )
  
  /* Verzióadatok elleörzése */
  $adat = mysql_fetch_array($sql->Lekerdezes("SELECT * FROM " .$cfg['tbprf']."version"), MYSQL_ASSOC);
- if ( ($adat["RELEASE_TYPE"] != RELEASE_TYPE) || ($adat["VERSION"] != VERSION) )
-	Hibauzenet("CRITICAL", "A futó verzió nem egyezik a telepített verzióval", "Futó verzió: <b>" .RELEASE_TYPE. " " .VERSION. " (" .RELEASE_DATE. ")</b><br>Telepített verzió: <b>" .$adat['RELEASE_TYPE']. " " .$adat['VERSION']. " (" .$adat['RELEASE_DATE']. ")</b><br>Bővebb információ: <a href='includes/help.php?cmd=Update' target='_blank'>kattints ide</a>");
- 
+ if ( ($adat["RELEASE_TYPE"] != RELEASE_TYPE) || ($adat["VERSION"] != VERSION) || ($adat["RELEASE_DATE"] != RELEASE_DATE) )
+	Hibauzenet("CRITICAL", "A futó verzió nem egyezik a telepített verzióval", "Futó verzió: <b>" .RELEASE_TYPE. " " .VERSION. " (" .RELEASE_DATE. ")</b><br>Telepített verzió: <b>" .$adat['RELEASE_TYPE']. " " .$adat['VERSION']. " (" .$adat['RELEASE_DATE']. ")</b><br>Bővebb információ: 
+	<a href='' onClick=\"window.open('includes/help.php?cmd=Update', 'popupwindow', 'width=570,height=320'); return false;\">kattints ide</a>");
+	
+
  if ($pagename != "admin.php") // Az admin.php-n ezeknek NEM kell megjelenniük
  {
 	Fejlec(); // Fejléc
@@ -79,8 +98,6 @@ function DoFooter() // Középső rész elküldése után
  
  global $templates;
  $templates->DoRight(); // Jobb oldali modulok
- 
- print("</div>"); // Dobozzárás
  
  Lablec(); // Lábléc
 }
