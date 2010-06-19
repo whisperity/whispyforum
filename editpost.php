40217faf764927faaf5e99b26507f5b8d878f18a
Whisperity <whisperity@gmail.com>
2010-06-19 06:41:46 +0000

    New picture added for post edition icon New file (only base loadings) added for post edition script Added new picture to images.lst Added both new files to export list

---
+++ editpost.php
@@ -0,0 +1,15 @@
+﻿<?php
+/* WhispyForum CMS-forum portálrendszer
+   http://code.google.com/p/whispyforum/
+*/
+
+/* ucp.php
+   felhasználói vezérlőpanel
+*/
+ 
+ include('includes/common.php'); // Betöltjük a portálrendszer alapscriptjeit (common.php elvégzi)
+ Inicialize('ucp.php');
+ SetTitle("Felhasználói vezérlőpult");
+ 
+ DoFooter();
+?>
