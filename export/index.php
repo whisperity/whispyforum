6a39a05168ad0f791d589d002a2dd2c26c0d4b02
Whisperity <whisperity@gmail.com>
2010-06-21 15:51:07 +0000

    index page

---
+++ export/index.php
@@ -0,0 +1,18 @@
+﻿<?php
+/* WhispyForum CMS-forum portálrendszer
+   http://code.google.com/p/whispyforum/
+*/
+
+/* export/index.php
+   kód exportáló, kezdőoldal
+*/
+ include('../config.php');
+ print("<link rel='stylesheet' type='text/css' href='../themes/" .THEME_NAME. "/style.css'>
+");
+ include('../includes/forms.php');
+ global $forms;
+ 
+ $forms->StartForm("POST", "exp.php");
+ $forms->UrlapElem("submit", "submit", "Exportálás megkezdése", 25, "Az fórumrendszer az előre beállított lista alapján a következő mappába kerül exportálásra: export_" .time(). "\ ", FALSE, time(), "A fenti szám UNIX-timestamp formátumú, a fájlok az exportálás megkezdésekori timestampre mentődnek le");
+ $forms->EndForm();
+?>
