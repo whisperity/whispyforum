2c40c1bda479991f05b4a7511f9bc143765037b2
Whisperity <whisperity@gmail.com>
2010-07-04 11:36:42 +0000

    Created settings.php for sample addon to write the sample text Sample addon sample text (settings variable) will be printed by the module

--- addons/sample/samplemodule.php
+++ addons/sample/samplemodule.php
@@ -6,8 +6,11 @@
 /* /addons/sample/samplemodule.php
    példaaddon példa modulja
 */
+ include("settings.cfg"); // Addon konfigurációs fájl betöltése (szükséges)
+ 
  print("<div class='menubox'><span class='menutitle'>Példaaddon</span><br>
 	Üdvözöllek a weboldalon!<br>");
 	SampleAddonFunction(); // Meghívjuk a sample.php functionjét
+	print("<br>" .$peldaaddon_sajatszoveg); // addons/sample/settings.cfg -vel létrehozott változó tartalmazza a kívánt szöveget
  print("</div>"); // A doboz lezárása
 ?>
---
+++ addons/sample/settings.php
@@ -0,0 +1,29 @@
+﻿<?php
+/* WhispyForum CMS-forum portálrendszer
+   http://code.google.com/p/whispyforum/
+*/
+
+/* /addons/sample/settings.php
+   példaaddon beállításfájla
+*/
+ include_once('addons/sample/settings.cfg'); // Szükséges az aktuális beálltások megtekintéséhez
+ 
+ if ( $_POST['setchange'] == "igaz" ) // Ha változtattunk az adatokon
+ {
+	file_put_contents("addons/sample/settings.cfg", "<?php\r\n\$peldaaddon_sajatszoveg = '" .$_POST['sajatszoveg']. "';\r\n?>"); // Fájl írása
+	/* Értesítés, további kódok lefutásának megakadályozása */
+	print("<div class='messagebox'>Addon beállításai sikeresen módosítva!<br><a href='admin.php?site=addons'>Visszatérés az addonok listájához</a></div>");
+	print("</td><td class='right' valign='top'>");
+	Lablec();
+	die();
+ }
+ 
+ print("<form method='POST' action='" .$_SERVER['PHP_SELF']. "'>
+	<p class='formText'>Saját megjelenítendő szöveg a példaaddonban: <input type='text' name='sajatszoveg' value='" .$peldaaddon_sajatszoveg. "'>
+	<input type='hidden' name='site' value='addons'>
+	<input type='hidden' name='action' value='settings'>
+	<input type='hidden' name='id' value=" .$addonid. ">
+	<input type='hidden' name='setchange' value='igaz'><br>
+	<input type='submit' value='Beállítások módosítása'></p>
+</form>"); // Űrlap
+?>
