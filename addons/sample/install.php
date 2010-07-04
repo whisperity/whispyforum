6c1f3325ded14ae96ccd5afe80598174a8c11879
Whisperity <whisperity@gmail.com>
2010-07-04 10:38:22 +0000

    Created sample addon install script

--- addons/sample/install.php
+++ addons/sample/install.php
@@ -6,7 +6,52 @@
 /* /addons/sample/install.php
    addon telepítő/eltávolító scriptje
 */
- //$sql->Lekerdezes("INSERT INTO " .$cfg['tbprf']."addons(subdir, name, descr, author, authoremail) VALUES ('sample', 'Példa addon', 'Példa addon az addonok működésének bemutatására', 'whisperity', 'whisperity@gmail.com')");
+ 
+ function Install() // Telepítési script
+ {
+	global $cfg, $sql;
+	
+	/* Az addon telepítési pozícióját POST-ban kapjuk */
+	$position = $_POST['position'];
+	if ( $position == $NULL)
+		$position = 0;
+		
+	/* Az addon telepítettségére történő ellenörzés a főkódban (admin/addons.php) már megtörtént */
+	/* Addon almappa ellenörzése */
+	if ( $_POST['addonsubdir'] != "sample" )
+	{
+		Hibauzenet("ERROR", "Érvénytelen addon almappa", "Az általad megadott addon telepítési almappája: " .$_POST['addonsubdir']. ", azonban ez az addon a <b>sample</b> almappából történő futtatást követeli meg.");
+		DoFooter();
+		die();
+	}
+	
+	switch ( $position ) { // A pozíció alapján döntjük el, mi fusson le
+		case 0:
+			print("<dl>
+				<dt>Az addon adatai</dt>
+					<dd>Név: Példaaddon</dd>
+					<dd>Almappa: sample</dd>
+					<dd>Fájlméret: " .DecodeSize(Addonmeret("sample")). "</dd>
+					<dd>Szerző: <a href='mailto:whisperity@gmail.com'>whisperity</a></dd>
+				<dt>Leírás:</dt>
+					<dd>Példa addon az addonok működésének bemutatására</dd>
+			</dl>
+			<form method='POST' action='" .$_SERVER['PHP_SELF']. "'>
+				<input type='hidden' name='site' value='addons'>
+				<input type='hidden' name='action' value='install_script'>
+				<input type='hidden' name='addonsubdir' value='sample'>
+				<input type='hidden' name='position' value=1>
+				<input type='submit' value='Az addon telepítéséhez nyomd meg ezt a gombot'>
+			</form>"); // Kiírjuk az addon adatait, valamint egy űrlapot a továbblépéshez
+		case 1:
+			/* Addon telepítése (sql-lekérdezések) */
+			
+			$sql->Lekerdezes("INSERT INTO " .$cfg['tbprf']."addons(subdir, name, descr, author, authoremail) VALUES ('sample', 'Példa addon', 'Példa addon az addonok működésének bemutatására', 'whisperity', 'whisperity@gmail.com')");
+			$sql->Lekerdezes("INSERT INTO " .$cfg['tbprf']."modules(name, type, side) VALUES ('sample/samplemodule.php', 'addonmodule', 2)");
+			
+			print("<br>Az addon telepítése sikeres volt. <a href='admin.php?site=addons'>Visszatérés az addonok listájához</a>"); // A felhasználó értesítése a sikeres telepítésről
+	}
+ }
  
  function Uninstall() // Eltávolítási script
  {
