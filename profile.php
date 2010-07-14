7d5c91f4d63f13e87cde547b9cb787aa350df525
Whisperity <whisperity@gmail.com>
2010-07-14 08:30:37 +0000

    Implemented user profiles, and user control panel UCP handles theme-selection (new method to list directory content dinamically) Added preview picture for DEFAULT theme Added nopreview picture to show as previews if particular theme does not have a preview picture

--- profile.php
+++ profile.php
@@ -10,6 +10,7 @@
  include('includes/common.php'); // Betöltjük a portálrendszer alapscriptjeit (common.php elvégzi)
  Inicialize('profile.php');
  
+ /* Csak akkor jelenik meg tartalom, ha a lekérdező felhasználó be van jelentkezve, és megadta a lekérdezni kívánt felhasználó id-jét */
  if ( $_GET['id'] == $NULL ) {
 	Hibauzenet("CRITICAL", "Érvénytelen paraméterek", "A kívánt felhasználó ID-jét kötelező megadni!");
 	DoFooter();
@@ -18,10 +19,103 @@
  
  if ( $_SESSION['userLevel'] == 0 )
  {
-	Hibauzenet("BAN");
+	Hibauzenet("BAN", "Be kell jelentkezned a felhasználói profilok megtekintéséhez");
 	DoFooter();
 	die();
  }
  
+ $felhasznalo = mysql_fetch_assoc($sql->Lekerdezes("SELECT * FROM " .$cfg['tbprf']."user WHERE id='" .$_GET['id']. "'")); // Adatok bekérése a megadott felhasználóról
+ 
+ if ( $felhasznalo == FALSE )
+ {
+	SetTitle("Nincs ilyen ID-jű felhasználó");
+	Hibauzenet("ERROR", "Nincs ilyen ID-jű felhasználó: " .$_GET['id'], "A megadott felhasználó nem létezik");
+	DoFooter();
+	die();
+ }
+ SetTitle("Profil megtekintése: " .$felhasznalo['username']);
+ 
+ print("<center><h2 class='header'>Profil megtekintése: " .$felhasznalo['username']. "</h2></center>
+<table><tr><td style='width: 50%' valign='top'>
+<div class='userbox'><span class='formHeader'> </span><p class='formText'>Felhasználó neve: " .$felhasznalo['username']. "<br>");
+
+	if ( $felhasznalo['realName'] != $NULL ) // Valódi név kiírása csak akkor, ha van valódi név
+		print("Valódi neve: " .$felhasznalo['realName']. "<br>");
+		
+print("E-mail címe: ");
+	
+	if ( $felhasznalo['showemail'] == 1 ) // A felhasználó e-mail címének megjelenítése csak akkor, ha ő ezt kérte
+	{
+		$usremail = str_replace("@", " (kukac) ", $felhasznalo['email']);
+		$usremail = str_replace(".", " (pont) ", $usremail);
+		print($usremail);
+	} else {
+		print("nem publikus");
+	}
+ 
+ print("<br>
+Regisztráció időpontja: " .Datum("normal","kisbetu","dL","H","i","s", $felhasznalo['regdate']). "<br>
+Legutoljára bejelentkezett: ");
+	
+	if ( $felhasznalo['lastlogintime'] == 0 ) // Ha a felhasználó UNIX idő szerint 0-kor (ergó: soha) nem lépett be
+	{
+		print("<span style='color: red; font-weight: bold'>soha</span>"); // azt írjuk ki
+	} else {
+		// Ha nem, akkor a tényleges időt, magyar dátummá formázva
+		print(Datum("normal","m","d","H","i","", $felhasznalo['lastlogintime']));
+	}
+	
+	if ( ($felhasznalo['loggedin'] == 1) && ($felhasznalo['cursessid'] != $NULL) && ($felhasznalo['curip'] != "0.0.0.0" ) ) // Ha a felhasználó jelenleg be van lépve, kiírjuk
+		print("&nbsp;&nbsp;<span style='color: darkgreen; font-weight: bold'>(jelenleg is itt tartózkodik)</span>");
+	
+print("<br>
+");
+ 
+ print("</p></td><td style='width: 50%' valign='top'><div class='userbox'><span class='formheader'> </span>
+<p class='formText'>Használt téma: <a href='themes/");
+	
+	if ( $felhasznalo['theme'] == $NULL ) // Ha a felhasználónál nincs beálított téma, a defaultot használja
+	{
+		$usrtema = "default";
+	} else {
+		// Ha van, kiírjuk a nevét
+		$usrtema = $felhasznalo['theme'];
+	}
+ print($usrtema. "/index.php'>" .$usrtema. "</a><br>
+Hozzászólások száma: " .$felhasznalo['postCount']. "");
+ print("</p></div></td></tr></table>
+ <div class='userbox'>
+ <p class='formText'>");
+	
+	switch ($felhasznalo['userLevel']) // Beállítjuk a szöveges userLevel értéket (userLevelTXT)
+	{
+		case 0:
+			$usrRang = 'Nincs aktiválva';
+			break;
+		case 1:
+			$usrRang = 'Felhasználó';
+			break;
+		case 2:
+			$usrRang = 'Moderátor';
+			break;
+		case 3:
+			$usrRang = 'Adminisztrátor';
+			break;
+	}
+	print("Rang: " .$usrRang. "<br>");
+	
+	if ( $felhasznalo['bemutatkozas'] != $NULL ) // Ha a felhasználónak van bemutatkozása, kiírjuk
+	{
+		$bemutatkozas = $felhasznalo['bemutatkozas']; // Nyers
+		$bemutatkozas = EmoticonParse($bemutatkozas); // Hangulatjelek hozzáadása BB-kódként
+		$bemutatkozas = HTMLDestroy($bemutatkozas); // HTML kódok nélkül 
+		$bemutatkozas = BBDecode($bemutatkozas); // BB kódok átalakítása HTML-kóddá (hangulatjeleket képpé)
+		print("Bemutatkozás: " .$bemutatkozas. "<br>");
+	}
+	
+	if ( $felhasznalo['thely'] != $NULL ) // Ha van tartózkodási hely megadva, kiírjuk
+		print("Tartózkodási hely: " .$felhasznalo['thely']. "<br>");
+ 
+ print("</div></p>");
  DoFooter();
 ?>
--- ucp.php
+++ ucp.php
@@ -11,5 +11,127 @@
  Inicialize('ucp.php');
  SetTitle("Felhasználói vezérlőpult");
  
+ print("<center><h2 class='header'>Felhasználói vezérlőpult</h2></center>");
+ $felhasznalo = mysql_fetch_assoc($sql->Lekerdezes("SELECT * FROM " .$cfg['tbprf']."user WHERE id='" .$_SESSION['userID']. "'"));
+ print("<div class='menubox'><a href='ucp.php?set=theme' class='menuItem'>Téma módosítása</a>&nbsp;<a href='ucp.php?set=bemut_thely' class='menuItem'>Egyéb adatok szerkesztése</a></div><br>");
+ 
+ if ( $_POST['set'] != $NULL )
+ {
+	// Ha POST-tal érkeznek az adatok, a POST site lesz az érték
+	$gpset = $_POST['set'];
+ } else {
+	// Ha nem post, akkor vagy GET-tel jött az adat, vagy sehogy
+	if ( $_GET['set'] != $NULL )
+	{
+		// Ha gettel érkezik, az lesz az érték
+		$gpset = $_GET['set'];
+	} else {
+		// Sehogy nem érkezett adat
+		$gpset = $NULL;
+	}
+ }
+ //print(str_replace("\n", "\n<br>", var_export($felhasznalo, TRUE)));
+ global $tdszam;
+ $tdszam = 0;
+ 
+ function UjTd()
+ {
+	global $tdszam;
+	if ( $tdszam == 2)
+	{
+		print("</tr><tr>");
+		$tdszam = 0;
+	}
+ }
+ 
+ switch ( $gpset )
+ {
+	case "theme":
+		
+		if ( ($_GET['seta'] == "settheme") && ($_GET['themename'] != $NULL) )
+		{
+			$sql->Lekerdezes("UPDATE " .$cfg['tbprf']."user SET theme='" .mysql_real_escape_string($_GET['themename']). "' WHERE id='" .$_SESSION['userID']. "'");
+			print("<div class='messagebox'>A témát sikeresen módosítottad!</a>");
+			DoFooter();
+			die();
+		}
+		
+		if ( $felhasznalo['theme'] == $NULL ) // Ha a felhasználónál nincs beálított téma, a defaultot használja
+		{
+			$usrtema = "default";
+		} else {
+			// Ha van, kiírjuk a nevét
+			$usrtema = $felhasznalo['theme'];
+		}
+		
+		print("Aktuális használt téma: <a href='themes/" .$usrtema. "/index.php'>" .$usrtema. "</a><br>");
+		
+		/* Elérhető témák listázása egy eddig ki nem próbált algoritmussal */
+		$dir = "./themes/"; // Téma gyökérmappa
+		$exempt = array('.', '..', '.svn', '_svn'); // Megadunk egy le nem kérdezendő kivétellistát
+		print("<table border='1'><tr>");
+		if (is_dir($dir)) 
+		{
+			if ($dh = opendir($dir))
+			{
+				while (($file = readdir($dh)) !== false)
+				{
+					if(!in_array(strtolower($file),$exempt))
+					{
+						if ( filetype($dir . $file) == "dir" )
+						{
+							/* Bekértük az összes MAPPÁT a themes mappából */
+							if ( file_exists($dir . $file . "/style.css") )
+							{
+								// Csak olyan mappákkal foglalkozunk, ahol van STYLE.CSS (tehát tud témát leírni)
+								print("<td style='widht: 50%'>");
+								
+								if ( file_exists($dir . $file . "/preview.jpg") )
+								{
+									// Ha van előnézeti kép, megjelenítjük azt is
+									print("<a href='" .$dir . $file . "/preview.jpg' alt='" .$file. " téma előnézete'><img src='" .$dir . $file . "/preview.jpg' width='320' height='240' alt='" .$file. " téma előnézete' border='0'></a>");
+								} else {
+									// Ha nincs, a "nincs előnézeti kép" képet jelenítjük meg
+									print("<img src='themes/nopreview.jpg'>");
+								}
+								
+								print("<br style='clear: both'>" .$file ."&nbsp;<a href='ucp.php?set=theme&seta=settheme&themename=" .$file. "'>Téma beállítása</a></td>");
+								$tdszam++;
+								UjTd();
+							}
+							echo "<br>";
+						}
+					}
+				}
+				closedir($dh);
+			}
+		}
+		print("</tr></table>");
+		//print("<a href='ucp.php?set=theme&seta=settheme'></a>");
+		break;
+	default:
+		
+		switch ($felhasznalo['userLevel']) // Beállítjuk a szöveges userLevel értéket (userLevelTXT)
+		{
+			case 0:
+				$usrRang = 'Nincs aktiválva';
+				break;
+			case 1:
+				$usrRang = 'Felhasználó';
+				break;
+			case 2:
+				$usrRang = 'Moderátor';
+				break;
+			case 3:
+				$usrRang = 'Adminisztrátor';
+				break;
+		}
+ 
+		print("<div class='menubox'><span class='menutitle'>Információk</span><br>
+		<p class='formText'>
+			<b>Regisztráció időpontja:</b> " .Datum("normal","kisbetu","dL","H","i","s", $felhasznalo['regdate']). "<br>
+			<b>Aktiválás időpontja:</b> " .Datum("normal","kisbetu","dL","H","i","s", $felhasznalo['activatedate']). "<br>
+			<b>Rang:</b> " .$usrRang. "</p></div>");
+ }
  DoFooter();
 ?>
