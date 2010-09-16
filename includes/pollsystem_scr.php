941d5423e9a30fcb6f3cb9c0da2ee971b8d89bcf
Whisperity <whisperity@gmail.com>
2010-09-16 16:36:29 +0000

    Fixed typo

--- includes/pollsystem_scr.php
+++ includes/pollsystem_scr.php
@@ -94,7 +94,7 @@
 	{
 		$lehetoseg = mysql_fetch_assoc($sql->Lekerdezes("SELECT * FROM " .$cfg['tbprf']."poll_opinions WHERE pollid='" .mysql_real_escape_string($pollid). "' AND opinionid='" .$i. "'")); // Lehetőség adatainak bekérése
 		
-		$szavazatszam = $sql->Lekerdezes("SELECT * FROM " .$cfg['tpbrf']."votes_cast WHERE pollid='" .mysql_real_escape_string($pollid). "' AND opinionid='" .mysql_real_escape_string($lehetoseg['opinionid']). "'"); // Szavazatok bekérése
+		$szavazatszam = $sql->Lekerdezes("SELECT * FROM " .$cfg['tbprf']."votes_cast WHERE pollid='" .mysql_real_escape_string($pollid). "' AND opinionid='" .mysql_real_escape_string($lehetoseg['opinionid']). "'"); // Szavazatok bekérése
 		
 		$lehetosegszamok[$lehetoseg['opinionid']] = array('text' => $lehetoseg['opinion'], 'count' => 0); // Szöveg beállítása
 		
@@ -106,48 +106,6 @@
 	}
 	
 	print("</ul>");
-	/*print("Grafikonon:\n<br>");
-	// Megkaptuk az $lehetosegszamok tömb $lehetoseg['opinionid'] tömbjében két értéket:
-	// text = lehetőség neve
-	// count = szavazatok száma
-	$wf_debug->RegisterDLEvent("Lehetőség és szavazatszámlálás megtörtént");
-	
-	// A Google Chart API segítségével létrehozzuk a táblázatot
-	$wf_debug->RegisterDLEvent("Google Chart kirajzolása...");
-	print("<img src='http://chart.apis.google.com/chart?\nchf=bg,s,ABCDEF\n&cht=bhs\n&chco=CCCCCC\n&chs=275x175\n&chd=t:"); // Bevezető adatok (típus, méret) és az adatsorok bevezetése
-	
-	$maxszavszam = 0; // Megnézzük, melyik a legtöbb szavazatot kapott lehetőség. Alapból 0. (Később a skálázáskor használjuk)
-	for ($i = 1; $i <= $szavazas['opcount']; $i++) { // Beírjuk a kódba a szavazatok számát
-		print($lehetosegszamok[$i]['count']); // Beküldjük az adatot
-		
-		// Ha az aktuálisan számolt lehetőség több szavazatot kapott mint az előző, ez lesz a legmagasabb
-		if ($lehetosegszamok[$i]['count'] > $maxszavszam)
-			$maxszavszam = $lehetosegszamok[$i]['count'];
-		
-		if ($i != $szavazas['opcount']) {
-			print (","); // Ha az adat nem az utolsó, beírunk egy vesszőt (,)
-		}
-	}
-	
-	print("\n&chxt=y,x\n&chds=0," .$maxszavszam. "\n&chxl=0:|"); // Google Chart API címke fejléc, max hossz
-	
-	for ($j = 1; $j <= $szavazas['opcount']; $j++) { // Beírjuk a kódba a lehetőségek nevét
-		print($lehetosegszamok[$j]['text']);
-		if ($j != $szavazas['opcount']) {
-			print ("|"); // Ha az adat nem az utolsó, beírjuk az elválasztó karaktert
-		}
-	}
-	
-	print("|1:");
-	
-	for ($k = 0; $k <= ($maxszavszam-1); $k++) { // Megcimkézzük a skálát
-		print("|".$k);
-	}
-	
-	print("|" .$maxszavszam. "'>\n</div>");
-	
-	$wf_debug->RegisterDLEvent("Google Chart grafikon rajzolása megtörtént");
-	*/
  }
  
  function PS_CheckUserVoteOnPoll($pollid) // Megnézzük, hogy a felhasználó szavazott-e már
