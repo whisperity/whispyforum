df2bc30a3105a13e243ee5c3a2e5382adcbddaed
Whisperity <whisperity@gmail.com>
2010-09-26 18:54:41 +0000

    Added two new smiles

--- includes/versions.php
+++ includes/versions.php
@@ -9,6 +9,6 @@
 
 	/* Verzióinformációk beállítása */
 	define('RELEASE_TYPE', 'revision'); // Kiadás típusa (dev-fejlesztői, rc-kiadásra jelölt, pre-kiadás előtti, beta, stable-stabil, ...)
-	define('VERSION', '331'); // Verziószám
+	define('VERSION', '333'); // Verziószám
 	define('RELEASE_DATE', "folyamatos"); // Kiadás dátuma (nem unix időben)
 ?>
