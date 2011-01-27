9446dd4e6f93eb46b88d68816701c142d443522e
Whisperity <whisperity@gmail.com>
2011-01-27 20:00:07 +0000

    Modified generateHexToken() a little bit: it will now be more random to generate more UUID-ish UUIDs.

--- includes/functions.php
+++ includes/functions.php
@@ -14,7 +14,7 @@
 	* 
 	* @outputs: a token in this format: 3c43:a968:3f69:3480:32fe:1206:d835
 	*/
-	$token = md5(sha1(time()));
+	$token = md5( sha1( time()+rand(0, time()) ) );
 	
 	$tok = substr($token, 0, 4); // First 4 characters
 	for ($i = 5; $i <= 28; $i+=4 ) // Generate the rest with a FOR loop
