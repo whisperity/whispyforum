14b24b203e1a9f16f0c4d6c23e125d1dadcfa221
Whisperity <whisperity@gmail.com>
2011-02-05 18:26:14 +0000

    Added support to log out the user to users class.

--- includes/users.class.php
+++ includes/users.class.php
@@ -45,6 +45,28 @@ class class_users
 		$_SESSION['log_bool'] = FALSE;
 	}
 	
+	private function __destroySession()
+	{
+		/**
+		 * This function deletes the session array
+		 * 		and the session cookie.
+		 * Internal use only!
+		 */
+		
+		$_SESSION = array(); // We delete everything from the session
+		
+		// We also decide to remove the cookie data
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+				$params["secure"], $params["httponly"]
+			);
+		}
+		
+		session_destroy(); // We destroy the session
+	}
+	
 	private function __checkUserData()
 	{
 		/**
@@ -75,18 +97,40 @@ class class_users
 			$this->__doLoginForm(); // We create the login form
 		} elseif ( ( $_SESSION['log_status'] == "user" ) && ( $_SESSION['log_bool'] == TRUE ) )
 		{
-			//$this->__; // Do user form
+			$this->__doUserForm(); // Do user form
 		}
 	}
 	
 	private function __doLoginForm()
 	{
+		/**
+		 * This function generates the login form
+		 * Internal use only!
+		 */
+		
 		global $Ctemplate; // We need to declare the templates class
 		
 		// We generate the return link from the HTTP REQUEST_URI (so we passthru the GET array)
 		$returnLink = substr($_SERVER['REQUEST_URI'],1); // We crop the starting / from the returnLink
 		
 		$Ctemplate->useTemplate("user/loginform", array(
+			"RETURN_TO"	=>	$returnLink
+		), FALSE);
+	}
+	
+	private function __doUserForm()
+	{
+		/**
+		 * This function generates the logout form
+		 * Internal use only!
+		 */
+		
+		global $Ctemplate; // We need to declare the templates class
+		
+		// We generate the return link from the HTTP REQUEST_URI (so we passthru the GET array)
+		$returnLink = substr($_SERVER['REQUEST_URI'],1); // We crop the starting / from the returnLink
+		
+		$Ctemplate->useTemplate("user/logoutform", array(
 			"RETURN_TO"	=>	$returnLink
 		), FALSE);
 	}
@@ -114,12 +158,39 @@ class class_users
 			$_SESSION['log_status'] = "user";
 			$_SESSION['log_bool'] = TRUE;
 			
+			$Cmysql->Query("UPDATE users SET curr_ip='" .$_SESSION['curr_ip']. "', curr_sessid='" .$_SESSION['curr_sessid']. "', loggedin=1 WHERE id='" .$userDBArray['id']. "'"); // We update the database to enter the current session data
 			return TRUE; // Then return TRUE
 		} elseif ( $userDBArray == FALSE )
 		{
 			// If there was errors during the query (wrong name/password)
 			return FALSE; // We return false
 		}
+	}
+	
+	function Logout($username)
+	{
+		/**
+		 * This function makes the user logged out
+		 * 
+		 * @inputs: $username - (string) logout username
+		 */
+		
+		global $Cmysql; // We need to declare the mySQL class
+		
+		$bLogout = $Cmysql->Query("UPDATE users SET curr_ip='0.0.0.0', curr_sessid='', loggedin=0 WHERE id='" .$_SESSION['uid']. "'"); // Clear session connections from database
+		
+		if ( $bLogout == FALSE )
+		{
+			// If we cannot log out the user
+			return FALSE; // We return false to give an error
+		} elseif ( $bLogout == TRUE )
+		{
+			// If we successfully logged out the user
+			
+			$this->__destroySession(); // We clear the session
+			return TRUE; // We give success
+		}
+		
 	}
 }
 ?>
