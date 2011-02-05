5344fac1e1633e44ff01b0e6379f8a797dc049e5
Whisperity <whisperity@gmail.com>
2011-02-05 17:41:39 +0000

    Added user login process and templates.

--- login.php
+++ login.php
@@ -13,15 +13,32 @@ if ( $_POST == NULL )
 {
 	// If POST array is null (direct opening the page)
 	// we give error
-	echo "no post";
+	$Ctemplate->useStaticTemplate("user/login_err_nopost", FALSE);
+	
 	exit; // We terminate the script
+}
+
+if ( $_POST['returnto'] == NULL )
+{
+	// We can have errors with unentered variables
+	// if the script redirects the user to the login page
+	// without giving any POST causing annoying and unnecessary redirections
+	$returnURI = "index.php"; // Return URI is the homepage
+} else {
+	$returnURI = $_POST['returnto']; // Return URI is the original return page
 }
 
 if ( $_POST['user_loginname'] == NULL )
 {
 	// If user wants to login without username
 	// give error message
-	echo "no usrname";
+	
+	$Ctemplate->useTemplate("user/login_err_novalue", array(
+		"RETURN_TO_URL"	=>	$returnURI, // Return URI
+		"VARIABLE_HEADER"	=>	"Username", // Unentered variable (uppercase)
+		"VARIABLE_BODY"	=>	"username" // Unentered variable (lowercase)
+	), FALSE);
+	
 	exit; // We terminate the script
 }
 
@@ -29,7 +46,13 @@ if ( $_POST['user_password'] == NULL )
 {
 	// If user wants to login without entering password
 	// give error message
-	echo "no usrpwd";
+	
+	$Ctemplate->useTemplate("user/login_err_novalue", array(
+		"RETURN_TO_URL"	=>	$returnURI, // Return URI
+		"VARIABLE_HEADER"	=>	"Password", // Unentered variable (uppercase)
+		"VARIABLE_BODY"	=>	"password" // Unentered variable (lowercase)
+	), FALSE);
+	
 	exit; // We terminate the script
 }
 
@@ -41,11 +64,15 @@ $logsuccess = $Cusers->Login($_POST['user_loginname'], $_POST['user_password']);
 if ( $logsuccess == FALSE )
 {
 	// We output an error message
-	echo "ERROR";
+	$Ctemplate->useTemplate("user/login_error", array(
+		"RETURN_TO_URL"	=>	$returnURI, // Return URI
+	), FALSE);
 } elseif ( $logsuccess == TRUE )
 {
 	// We give success
-	echo "SUCCESS!";
+	$Ctemplate->useTemplate("user/login_success", array(
+		"RETURN_TO_URL"	=>	$returnURI, // Return URI
+	), FALSE);
 }
 
 DoFooter();
