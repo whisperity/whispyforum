9441268ba1bc4c63433bb6252fe396c8b406bb1a
Whisperity <whisperity@gmail.com>
2011-02-06 14:12:09 +0000

    Added html head tags to install templates.

--- index.php
+++ index.php
@@ -5,12 +5,5 @@
 
 include("includes/load.php"); // Load webpage
 
-$Ctemplate->useTemplate("example_template", array(
-	'TITLE'	=>	"Page title", // Head title and center title of the page
-	'SITENAME'	=>	"WhispyForum branch checkout", // Example sitename
-	'WORKING_COLOR'	=>	"#49A835",	// The color of the example template's word "working"
-	'WEIGHT'	=>	"bold" // font-weight of the "working" word. (normal/bold)
-	), FALSE ); // Use the example template file
-
 DoFooter();
 ?>
