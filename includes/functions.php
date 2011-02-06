2cc3e1ab41f5137f6b982b9b56dd4ac249f1f060
Whisperity <whisperity@gmail.com>
2011-02-06 11:33:06 +0000

    Added three function to save thumbnails from JPEG, GIF and PNG images. Updated function.php's documentation.

--- includes/functions.php
+++ includes/functions.php
@@ -25,4 +25,79 @@ echo date('l jS \of F Y H:i:s')." functions loaded\n<br>"; // DEV
 	
 	return $tok;
  }
+ 
+ function saveThumbnailJPEG($originalImage,$new_height,$filename)
+ {
+	/**
+	* This function resizes an image to a set height and saves it as a thumbnail
+	* Use for JPEG pictures
+	* 
+	* @inputs: $originalImage - filename of the original image file
+	* 		   $new_height - new height (in pixels)
+	*		   $filename - output filename
+	*/
+	
+    // Get the original geometry and calculate scales
+    list($width, $height) = getimagesize($originalImage);
+        $new_width = round(($width * $new_height) / $height);
+
+    // Resize the original image
+    $imageResized = imagecreatetruecolor($new_width, $new_height);
+    $imageTmp     = imagecreatefromjpeg ($originalImage);
+    imagecopyresampled($imageResized, $imageTmp, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    // Output
+    imagejpeg($imageResized, $filename.".jpg", 100);
+    imageDestroy($imageResized);
+ }
+ 
+ function saveThumbnailGIF($originalImage,$new_height,$filename)
+ {
+	/**
+	* This function resizes an image to a set height and saves it as a thumbnail
+	* Use for GIF pictures
+	* 
+	* @inputs: $originalImage - filename of the original image file
+	* 		   $new_height - new height (in pixels)
+	* 		   $filename - output filename
+	*/
+	
+    // Get the original geometry and calculate scales
+    list($width, $height) = getimagesize($originalImage);
+        $new_width = round(($width * $new_height) / $height);
+
+    // Resize the original image
+    $imageResized = imagecreatetruecolor($new_width, $new_height);
+    $imageTmp     = imagecreatefromgif ($originalImage);
+    imagecopyresampled($imageResized, $imageTmp, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    // Output
+    imagegif($imageResized, $filename.".gif", 0);
+    imageDestroy($imageResized);
+ }
+ 
+ function saveThumbnailPNG($originalImage,$new_height,$filename)
+ {
+	/**
+	* This function resizes an image to a set height and saves it as a thumbnail
+	* Use for GIF pictures
+	* 
+	* @inputs: $originalImage - filename of the original image file
+	* 		   $new_height - new height (in pixels)
+	* 		   $filename - output filename
+	*/
+	
+    // Get the original geometry and calculate scales
+    list($width, $height) = getimagesize($originalImage);
+        $new_width = round(($width * $new_height) / $height);
+
+    // Resize the original image
+    $imageResized = imagecreatetruecolor($new_width, $new_height);
+    $imageTmp     = imagecreatefrompng ($originalImage);
+    imagecopyresampled($imageResized, $imageTmp, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    // Output
+    imagepng($imageResized, $filename.".png", 0);
+    imageDestroy($imageResized);
+ }
 ?>
