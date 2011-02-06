39fad40880083b12031d73c73dec56722fbf3e5b
Whisperity <whisperity@gmail.com>
2011-02-06 18:31:42 +0000

    Added UCP footer. Added script to determine UCP subpage. Added giving error if user wants to view UCP without logging in.

--- control_user.php
+++ control_user.php
@@ -8,9 +8,46 @@
  */
 
 include("includes/load.php"); // Load webpage
+$Ctemplate->useStaticTemplate("user/cp_head", FALSE); // Header
 
-$Ctemplate->useStaticTemplate("user/cp_head", FALSE);
-echo '</td></tr></table>';
+// We define the $site variable
+$site = "";
 
+if ( $_SESSION['log_bool'] == FALSE )
+{
+	// If the user is a guest
+	$Ctemplate->useStaticTemplate("user/unaviable_guest", FALSE); // We give an unaviable error
+} elseif ( $_SESSION['log_bool'] == TRUE)
+{
+// If user is logged in, the control panel is accessible
+
+if ( isset($_POST['site']) )
+{
+	// If site is passed by POST
+	// the site variable is the POSTed value
+	
+	$site = $_POST['site'];
+} elseif ( !isset($_POST['site']) )
+{
+	// If the POSTed variable is NULL
+	// we see if there's site variable GET
+	
+	if ( isset($_GET['site']) )
+	{
+		// If there is, site is the GET value
+		$site = $_GET['site'];
+	} elseif ( !isset($_GET['site']) )
+	{
+		// If not, site is NULL
+		$site = NULL;
+	}
+}
+
+// Now, the site variable is either NULL or set from HTTP POST/GET
+
+echo $site;
+
+}
+$Ctemplate->useStaticTemplate("user/cp_foot", FALSE); // Footer
 DoFooter();
 ?>
