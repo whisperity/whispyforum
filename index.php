34e56bc3c63eeeade966bb33fac1c40a346df139
Whisperity <whisperity@gmail.com>
2012-04-14 09:03:38 +0000

    Dropped old Cusers, Cmysql and Ctemplates. Changed EOL characters to CR-LF (Windows-style).

--- index.php
+++ index.php
@@ -1,31 +1,9 @@
 <?php
 /**
  * WhispyForum placeholder index file
- */
+*/
 
-//include("includes/load.php"); // Load webpage
-define('WHISPYFORUM', TRUE);
-require("config.php");
-require("includes/functions.php");
-require("includes/mysql.php");
-require("includes/module.php");
-require("includes/template.php");
-require("includes/user.php");
+include("includes/load.php");
 
-global $template, $sql, $user;
-$template = new template();
-$sql = new mysql( $cfg['dbhost'], $cfg['dbuser'], $cfg['dbpass'], $cfg['dbname'] );
-$user = new user(0, FALSE);
-
-
-
-
-prettyVar($user);
-prettyVar($sql);
-prettyVar($template);
-
-unset($user);
-unset($sql);
-unset($template);
-//DoFooter();
+footer();
 ?>
