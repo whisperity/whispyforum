7daefaa44713b4f84f072689805531e20cd6a529
Whisperity <whisperity@gmail.com>
2012-04-15 16:46:30 +0000

    Changed the version of 0.5 'infant' to 0.1 'infant'. Updated related files.
    1.0 'Tuvia' is now 0.2 'Tuvia'.

--- example.framework.php
+++ example.framework.php
@@ -4,7 +4,7 @@ include('includes/load.php'); // Call the loader
 // Now, the center box is started, creating individual content
 $Ctemplate->useTemplate("wf_hello", array(
     'NAME'    =>    "Individual",
-	'VERSION'	=>	"1.0",
+	'VERSION'	=>	"0.2",
     'CODENAME'    =>    "Tuvia"
 ), FALSE);
 echo "<br>\n";
